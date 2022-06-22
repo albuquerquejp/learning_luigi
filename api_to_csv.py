39bf7c61b9e44aca7d0ea710220dc26e5e5ae579
João Paulo Albuquerque <101363298+albuquerquejp@users.noreply.github.com>
2022-06-22 17:10:38 -0300

    Update api_to_csv.py

--- api_to_csv.py
+++ api_to_csv.py
@@ -17,6 +17,38 @@ class MyTask1 (luigi.Task):
         
         with self.output().open('w') as csv: 
             csv.write(df.to_csv())
-   
+
+class MyTask2 (luigi.Task):
+    
+    def output(self):
+        return luigi.LocalTarget("ethereum_data.csv")
+
+    def run(self):
+        cg = CoinGeckoAPI()
+
+        ethereum_data = cg.get_coin_market_chart_by_id(id='ethereum', vs_currency='usd', days=90)
+
+        df = pd.DataFrame(ethereum_data)
+        
+        with self.output().open('w') as csv: 
+            csv.write(df.to_csv())
+
+class MyTask3 (luigi.Task):
+    
+    def output(self):
+        return luigi.LocalTarget("cardano_data.csv")
+
+    def run(self):
+        cg = CoinGeckoAPI()
+
+        cardano_data = cg.get_coin_market_chart_by_id(id='cardano', vs_currency='usd', days=90)
+
+        df = pd.DataFrame(cardano_data)
+        
+        with self.output().open('w') as csv: 
+            csv.write(df.to_csv())
+
+
+
 if __name__ == '__main__':
     luigi.run()
